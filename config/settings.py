2da92a7be3314e65876a3375fdf0eec4bd1ae377
Svetlana Kovakina <skovakina@gmail.com>
2025-04-25 14:34:00 -0700

    wip: set up models

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'planicorn',
+        'USER': 'postgres',
+        'PASSWORD': '393304',
     }
 }
 
---
+++ main_app/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Board model
+class Board(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+# Tag model
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tags')
+
+    def __str__(self):
+        return self.name
+
+# Event model
+class Event(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='events')
+    title = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField(blank=True)
+    tags = models.ManyToManyField(Tag, related_name='events', blank=True)  # <- added
+
+    def __str__(self):
+        return self.title
+
+# Profile model (extending User)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar_url = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+# Signal to automatically create/update profile
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
