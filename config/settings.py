6bc54877726873971bab49de8fd8c77a84d02e32
Svetlana Kovakina <skovakina@gmail.com>
2025-05-06 12:44:44 -0700

    add procfile

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
